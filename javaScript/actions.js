be2c2cfb0e059d8c2312296d11c57948bdf74cda
Brandon Richards <bqrichards@icloud.com>
2019-03-20 23:58:52 -0500

    Fix Schedule and Add Last Match Page

--- javaScript/actions.js
+++ javaScript/actions.js
@@ -1,7 +1,6 @@
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
+for (var i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
@@ -11,21 +10,12 @@ for (i = 0; i < myNodelist.length; i++) {
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
+for (var i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
   }
 }
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ol');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
@@ -40,13 +30,17 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
+  li.onclick = function() {
+    li.classList.toggle('checked');
+  };
+
+  var span = document.createElement("span");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
+  for (var i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
